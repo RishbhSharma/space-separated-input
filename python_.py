626e91baecc62b93910aaabda1075d846862bb71
Rishbh Sharma <rishbhsharma2008@gmail.com>
2016-06-20 19:55:34 +0530

    Create python_.py

---
+++ python_.py
@@ -0,0 +1,8 @@
+#Read space separated INTs and push into array
+A=map(int,raw_input().split())
+
+#Read two ints and map into different VARs
+a,b=map(int,raw_input().split())
+
+#Read different data-types and put into an array(list)
+C=map(eval,raw_input().split())
